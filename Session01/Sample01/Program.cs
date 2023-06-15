f394ab3bfae7d99cef509355844e0788d6ac6c84
Fatemeh Rostamipour <fatemehrostamih987@gmail.com>
2023-06-15 08:05:00 +0330

    Sample01-Session01-ShowingFiveNamesAphabetically

---
+++ Session01/Sample01/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter five names:");
+            string[] StudentNames = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                StudentNames[i] = Console.ReadLine();
+            }
+
+            Array.Sort(StudentNames);
+
+            Console.WriteLine("Showing the names alphabetically.");
+
+            foreach (var name in StudentNames)
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadLine();
+        }
+    }
+}
